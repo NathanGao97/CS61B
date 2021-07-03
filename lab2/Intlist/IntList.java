e18d19c4dad5ac1825c755e9b41d7bef3b1f844f
GZD <gaozedong1111@gmail.com>
2021-07-04 00:10:30 +0800

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,10 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null) {
+            A = B;
+            return A;
+        }
         IntList res = A;
         while (res.rest != null) {
             res = res.rest;
